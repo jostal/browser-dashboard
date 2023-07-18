5c713ea39e8b558b80f7d3f7fe567bb9b459c252
Joseph Talon <josephtalon01@gmail.com>
2023-07-18 16:29:27 -0400

    Render links from db

---
+++ svelte-dashboard/src/components/App.svelte
@@ -0,0 +1,33 @@
+<script>
+
+  export let app
+
+</script>
+
+<div class="app-wrapper">
+  <a href={app.url}>
+    <img src={`http://www.google.com/s2/favicons?sz=128&domain=${app.url}`} alt={`${app.name} icon`}/>
+    <label>{app.name}</label>
+  </a>
+</div>
+
+<style lang="scss">
+  .app-wrapper {
+    display: inline-block;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+
+      img {
+        width: 64px;
+        height: 64px;
+      }
+
+      label {
+        display: block;
+        text-align: center;
+      }
+    }
+  }
+</style>
